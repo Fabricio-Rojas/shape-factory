acc0d8b798c1087edd44532362cb7b161bc9227e
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-03-14 12:19:38 -0500

    added shape deploy function

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,7 +2,12 @@
 
 // Declaring Elements
 
+const createBtn = document.querySelector('#shape-create');
+const shapeSelect = document.querySelector('#shape-type');
+const colorSelect = document.querySelector('#shape-color');
 
+const grid = document.querySelector('.grid');
+const textDisplay = document.querySelector('.shape-txt');
 
 // Class Definition
 
@@ -36,3 +41,22 @@ class Shape {
 };
 
 // Main function
+let shapeList = [];
+let divCount = 0;
+createBtn.addEventListener('click', function() {
+    divCount++;
+    if (divCount >= 25) {
+        textDisplay.innerText = 'Reached max amount of shapes';
+        return;
+    };
+
+    const newShape = new Shape(shapeSelect.value, colorSelect.value);
+    shapeList.push(newShape);
+
+    const newDiv = document.createElement('div');
+    newDiv.classList.add(newShape.name);
+    
+    newDiv.style.backgroundColor = newShape.color;
+    grid.append(newDiv);
+});
+
