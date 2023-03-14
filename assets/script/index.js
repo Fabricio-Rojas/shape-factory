dafce73090c09388b73cd6b2b5f830b19f6e9fe1
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-03-14 10:46:29 -0500

    completed Shape class

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,6 +6,33 @@
 
 // Class Definition
 
+class Shape {
+    #name;
+    #color;
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    };
 
+    set name(name) {
+        this.#name = name;
+    };
+
+    set color(color) {
+        this.#color = color;
+    };
+
+    get name() {
+        return this.#name;
+    };
+
+    get color() {
+        return this.#color
+    };
+
+    getInfo() {
+        return `${this.#name} ${this.#color}`
+    };
+};
 
 // Main function
